883268ef082b22bbdc201eb76427d31044bd86e0
Oleh Perehuda <peregudson97@gmail.com>
2022-06-15 18:40:19 +0300

    feat(extension): route changed and styles

--- packages/blockchain-wallet-v4-frontend/src/routes/extensionRoutes/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/routes/extensionRoutes/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Send from 'blockchain-wallet-v4-frontend/src/scenes/extension/Send'
+import SendConfirm from 'blockchain-wallet-v4-frontend/src/scenes/extension/SendConfirm'
 import styled from 'styled-components'
 
 import HomeRoutes from './HomeRoutes'
 
 export const extensionHeight = 600
-export const extensionWidth = 800
+export const extensionWidth = 360
 
 const Wrapper = styled.div`
   display: flex;
@@ -29,6 +30,8 @@ const ExtensionRoutes = (props) => {
         <Switch>
           <Route path={`${path}/home`} component={HomeRoutes} />
           <Route path={`${path}/send`} component={Send} />
+          <Route path={`${path}/sending`} component={Sending} />
+          <Route path={`${path}/send-confirm`} component={SendConfirm} />
         </Switch>
       </InnerWrapper>
     </Wrapper>
--- packages/blockchain-wallet-v4-frontend/src/scenes/extension/Send/WalletItem/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/extension/Send/WalletItem/index.tsx
@@ -31,13 +31,7 @@ const WalletItem: React.FC<{ walletAddress: string }> = ({ walletAddress }) => {
     <WalletWrapper id='walletItem'>
       <IconBlockchainCircle height='32px' color='#473BCB' width='32px' />
       <AccountInfo>
-        <WalletAddressWrapper
-          size='12px'
-          lineHeight='18px'
-          color='white'
-          weight={500}
-          style={{ wordBreak: 'break-word' }}
-        >
+        <WalletAddressWrapper size='12px' lineHeight='18px' color='white' weight={500}>
           {walletAddress}
         </WalletAddressWrapper>
         <WalletAddressLabel size='12px' lineHeight='18px' weight={500}>
