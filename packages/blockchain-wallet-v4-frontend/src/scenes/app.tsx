34b7d18c46d89cf208869cecea3341a5999cf3e5
vladdezhniuk <vladdezhniuk@gmail.com>
2022-07-09 00:06:32 +0300

    feat(extnsion): fix

--- packages/blockchain-wallet-v4-frontend/src/scenes/app.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/app.tsx
@@ -268,7 +268,7 @@ const App = ({
                             hideMenu={coinViewV2}
                             center={coinViewV2}
                           />
-                          {/* {isAuthenticated ? <Redirect to='/home' /> : <Redirect to='/login' />} */}
+                          {isAuthenticated ? <Redirect to='/home' /> : <Redirect to='/login' />}
                         </Switch>
                       </Suspense>
                     </ConnectedRouter>
