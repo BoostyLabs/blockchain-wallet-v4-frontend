5b97a55d4f44e186c8fe4d8023a329534bb7a002
Oleh Perehuda <peregudson97@gmail.com>
2022-07-19 15:17:15 +0300

    feat(plugin): select address component to send scene added

---
+++ packages/blockchain-wallet-v4-frontend/src/scenes/plugin/Send/SelectAddress/AddressInput/template.succes.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { isValidAddress } from '@core/utils/eth'
+// @ts-ignore
+import { TextInput } from 'blockchain-info-components'
+
+const Wrapper = styled.div`
+  margin: 17px auto 0;
+  width: 100%;
+`
+
+const AddressInput = (props) => {
+  const { input, meta, ...rest } = props
+
+  return (
+    <Wrapper>
+      <TextInput {...input} {...meta} {...rest} />
+    </Wrapper>
+  )
+}
+
+export default AddressInput
