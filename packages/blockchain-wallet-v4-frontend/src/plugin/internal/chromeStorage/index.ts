3f219f16003e27a500cf3680afca1316b1224da5
bakhrom-akbarov <bakhrom.akbarov@gmail.com>
2022-07-20 14:27:59 +0500

    feat: chrome storage methods

--- packages/blockchain-wallet-v4-frontend/src/plugin/internal/chromeStorage/index.ts
+++ packages/blockchain-wallet-v4-frontend/src/plugin/internal/chromeStorage/index.ts
@@ -6,3 +6,18 @@ export const getPassword = async (): Promise<string> => {
 export const savePassword = async (password: string): Promise<void> => {
   await chrome.storage.session.set({ password })
 }
+
+export const setSessionExpireTime = async (): Promise<void> => {
+  const sessionExpiresAt = new Date()
+  sessionExpiresAt.setHours(sessionExpiresAt.getHours() + 2)
+  await chrome.storage.local.set({ sessionExpiresAt })
+}
+
+export const isSessionActive = async (): Promise<boolean> => {
+  const { sessionExpiresAt } = await chrome.storage.local.get('sessionExpiresAt')
+  return sessionExpiresAt > new Date()
+}
+
+export const saveSessionPayload = async (sessionPayload: any): Promise<void> => {
+  await chrome.storage.session.set({ sessionPayload })
+}
